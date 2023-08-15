449794db62aae9b7a9934a40d95094838e82d60e
VuDuc197 <ducvv.smile@gmail.com>
2023-08-15 17:06:26 +0700

    ducvv: helpdesk ticket 15/8

--- force-app/force-app/main/default/lwc/vmo_handleToTalTickets/vmo_handleToTalTickets.js
+++ force-app/force-app/main/default/lwc/vmo_handleToTalTickets/vmo_handleToTalTickets.js
@@ -1,31 +1,37 @@
+import getToTalClosed from '@salesforce/apex/vmo_CaseController.getToTalClosed';
+import getToTalOpen from '@salesforce/apex/vmo_CaseController.getToTalOpen';
+import getToTalProcessing from '@salesforce/apex/vmo_CaseController.getToTalProcessing';
+import getToTalThisMonth from '@salesforce/apex/vmo_CaseController.getToTalThisMonth';
+import getToTalTickets from '@salesforce/apex/vmo_CaseController.getToTalTickets';
 import { LightningElement, wire } from 'lwc';
-import {handleToTalTickets} from '@salesforce/apex/vmo_CaseController'
+
 export default class Vmo_handleToTalTickets extends LightningElement {
-    
-    //tổng số Case
-    handleToTalTickets( ) {
+    totalTickets = 0;
+    totalThisMonth = 0;
+    totalOpen = 0;
+    totalProcessing = 0;
+    totalClosed = 0;
 
-    }
-    //tổng số case trong tháng
-    handleTotalThisMonth() {
-
-    }
-    //tổng số case thỏa mãn đk Case.Status = "New"
-    handleTotalOpen() {
-
-    }
-    //tổng số case thỏa mãn đk Case.Status != "New" && Case.isClosed= false
-    handleTotalProcessing() {
-
-    }
-    //tổng số case thỏa mãn đk Case.isClosed= true
-    handleTotalClosed() {
-
-    }
     connectedCallback() {
+        this.fetchCaseData();
+    }
+    renderedCallback() {
 
     }
     disconnectedCallback() {
 
+    }
+    //fill data case
+    async fetchCaseData() { 
+        let res = await getToTalTickets();
+        this.totalTickets = res;
+        let res1 = await getToTalThisMonth();
+        this.totalThisMonth = res1;
+        let res2 = await getToTalOpen();
+        this.totalOpen = res2;
+        let res3 = await getToTalProcessing();
+        this.totalProcessing = res3;
+        let res4 = await getToTalClosed();
+        this.totalClosed = res4;
     }
 }
