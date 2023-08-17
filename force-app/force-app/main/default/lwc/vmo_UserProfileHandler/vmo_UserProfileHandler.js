ac235211d409e735513fc5892588527f0f0d07e9
VuDuc197 <ducvv.smile@gmail.com>
2023-08-17 17:22:33 +0700

    ducvv test 17/08

--- force-app/force-app/main/default/lwc/vmo_UserProfileHandler/vmo_UserProfileHandler.js
+++ force-app/force-app/main/default/lwc/vmo_UserProfileHandler/vmo_UserProfileHandler.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire ,api } from 'lwc';
+import User_Id from '@salesforce/schema/User.Id';
+import getUserInfor from '@salesforce/apex/vmo_UserController.getUserInfor';
+import { getRecord, getFieldValue  } from 'lightning/uiRecordApi';
+export default class Vmo_UserProfileHandler extends LightningElement {
+    @api recordId;
+    currentPhotoUrl ;
+    fullName;
+    birthDate;
+    staffCode;
 
-export default class Vmo_UserProfileHandler extends LightningElement {}
+    //fill data user
+    usrData = {};
+    //handler button Search
+    handleBtnSearch() {
+
+    }
+    connectedCallback() {
+        this.fetchUserData();
+    }
+    disconnectedCallback() {
+        
+    }
+    async fetchUserData () {
+        let res = await getUserInfor();
+        this.usrData = res;
+    }
+}
---
+++ force-app/force-app/main/default/lwc/vmo_handleToTalTickets/vmo_TicketRealtimeTrackingHandler.js
@@ -0,0 +1,52 @@
+import getToTalClosed from '@salesforce/apex/vmo_CaseController.getToTalClosed';
+import getToTalOpen from '@salesforce/apex/vmo_CaseController.getToTalOpen';
+import getToTalProcessing from '@salesforce/apex/vmo_CaseController.getToTalProcessing';
+import getToTalThisMonth from '@salesforce/apex/vmo_CaseController.getToTalThisMonth';
+import getToTalTickets from '@salesforce/apex/vmo_CaseController.getToTalTickets';
+import { LightningElement, wire } from 'lwc';
+
+export default class Vmo_handleToTalTickets extends LightningElement {
+    totalTickets = 0;
+    totalThisMonth = 0;
+    totalOpen = 0;
+    totalProcessing = 0;
+    totalClosed = 0;
+
+    connectedCallback() {
+        this.fetchCaseData();
+    }
+    renderedCallback() {
+
+    }
+    disconnectedCallback() {
+
+    }
+    //fill data case
+    /*async fetchCaseData() { 
+        let res =  getToTalTickets();
+        this.totalTickets = res;
+        let res1 = await getToTalThisMonth();
+        this.totalThisMonth = res1;
+        let res2 = await getToTalOpen();
+        this.totalOpen = res2;
+        let res3 = await getToTalProcessing();
+        this.totalProcessing = res3;
+        let res4 = await getToTalClosed();
+        this.totalClosed = res4;
+    }*/
+    async fetchCaseData() {
+        await Promise.all(getToTalTickets(), getToTalThisMonth(), getToTalOpen(), getToTalProcessing(), getToTalClosed()) 
+            .then(data => {
+                this.totalTickets = getToTalTickets();
+                this.totalThisMonth = getToTalThisMonth();
+                this.totalOpen = getToTalOpen();
+                this.totalProcessing = getToTalProcessing();
+                this.totalClosed = getToTalClosed();
+            })
+            .catch((err) => {
+                
+            }
+
+            )
+    }
+}
